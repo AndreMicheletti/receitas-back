9b54dc53dcb39adcdccaec43d7641bf51be491c8
AndreMicheletti <andreluizmtmicheletti@gmail.com>
2018-07-23 21:28:36 -0300

    add stop service script

--- start_service.sh
+++ start_service.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-docker kill receitas_back
-docker rm receitas_back
+./stop_service.sh
 docker build -t receitas_back .
 docker run -d -p 7465:7465 --name receitas_back receitas_back
---
+++ stop_service.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+docker kill receitas_back
+docker rm receitas_back
