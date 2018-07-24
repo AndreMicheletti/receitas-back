e11c94e218e2bb61185a5ff5a24baf27e95c054e
AndreMicheletti <andreluizmtmicheletti@gmail.com>
2018-07-23 22:03:31 -0300

    because it was running with gunicorn, __main__ was not called

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import os
-
 from flask import Flask, Blueprint
 from flask_restful import Api
+from mongoengine import register_connection
 
 from resources.recipes import RecipeAPI
 from resources.ingredients import IngredientAPI
@@ -14,6 +13,11 @@ app.config["MONGODB_SETTINGS"] = FLASK_MONGODB_SETTINGS
 
 app_blueprint = Blueprint('api', __name__, url_prefix='/api')
 
+# CONNECT WITH DATABASE
+
+register_connection('default', **FLASK_MONGODB_SETTINGS)
+db.init_app(app)
+
 # register APIs
 api_v1 = Api(app_blueprint, prefix='/v1')
 
@@ -24,6 +28,5 @@ api_v1.add_resource(IngredientAPI, '/ingredient/<category>')
 app.register_blueprint(app_blueprint)
 
 if __name__ == '__main__':
-    db.init_app(app)
 
     app.run(host="0.0.0.0", port='7465', debug=False)
